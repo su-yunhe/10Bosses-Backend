79729045fade6f5d8b81abf403f41e9eda4d74c8
maybeAlways <112628774+maybeAlways@users.noreply.github.com>
2024-06-25 17:05:52 +0800

    基本函数（未测

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path("manager_register", register),
-    # path("manager_login", login),
+    path("update_enterprise", update_enterprise),
+    path("show_enterprise", show_enterprise),
 ]
