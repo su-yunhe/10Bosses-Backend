88445bd7928f3ebba555fd6f1c397583e05aef09
xfy-mufc <123456>
2024-06-26 15:45:12 +0800

    小修改

--- enterprise/views.py
+++ enterprise/views.py
@@ -5,8 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import Enterprise
 from recruit.models import Recruit
-from haystack.forms import ModelSearchForm
-from haystack.query import EmptySearchQuerySet, SearchQuerySet
+from haystack.query import SearchQuerySet
 
 import base64
 from django.shortcuts import get_object_or_404
@@ -18,8 +17,8 @@ from Users.models import Applicant
 
 @csrf_exempt
 def enterprise_search(request):
-    if request.method == 'POST':
-        query = (request.POST.get('q', ''))  # 从POST请求中获取查询参数
+    if request.method == "POST":
+        query = request.POST.get("q", "")  # 从POST请求中获取查询参数
         if query:
             # jieba拆分搜索关键字
             search_list = jieba.cut_for_search(query)
@@ -48,14 +47,16 @@ def enterprise_search(request):
                 enterprise["manager_name"] = manager_name
                 del enterprise["manager_id"]
                 # 获取企业招聘列表
-                recruitments = list(Recruit.objects.values().filter(enterprise_id=enterprise_id))
+                recruitments = list(
+                    Recruit.objects.values().filter(enterprise_id=enterprise_id)
+                )
                 for recruitment in recruitments:
                     rec_enter_id = recruitment["enterprise_id"]
                     enterprise_name = Enterprise.objects.get(id=rec_enter_id).name
                     recruitment["enterprise_name"] = enterprise_name
                 # print(recruitments)
                 results.append({"enterprise": enterprise, "recruitment": recruitments})
-            return JsonResponse({'results': results}, status=200)
+            return JsonResponse({"results": results}, status=200)
         else:
             # 用户没有提供关键词,只返回一些招聘
             recruitments = list(Recruit.objects.values().all())
@@ -64,34 +65,38 @@ def enterprise_search(request):
                 enterprise_name = Enterprise.objects.get(id=rec_enter_id).name
                 recruitment["enterprise_name"] = enterprise_name
             print(recruitments)
-            return JsonResponse({'results': recruitments}, status=200)
+            return JsonResponse({"results": recruitments}, status=200)
     return JsonResponse({"errno": 2001, "msg": "请求方式错误"})
 
 
 @csrf_exempt
 def whoosh_search(request):
-    query = request.POST.get('q', '')
+    query = request.POST.get("q", "")
     sqs = SearchQuerySet().filter(content=query)
     search_results = list()
     for result in sqs:
         search_results.append(result.object.id)
     print(search_results)
-    results = [{'name': result.object.name} for result in sqs]
+    results = [{"name": result.object.name} for result in sqs]
     return JsonResponse(results, safe=False)
 
 
 @csrf_exempt
 def get_enterprise_recruitment(request):
     # 获取企业招聘信息
-    if request.method == 'POST':
-        enterprise_id = request.POST.get('enterprise_id')
+    if request.method == "POST":
+        enterprise_id = request.POST.get("enterprise_id")
         print(enterprise_id)
-        recruitment_list = list(Recruit.objects.values().filter(enterprise_id=enterprise_id))
+        recruitment_list = list(
+            Recruit.objects.values().filter(enterprise_id=enterprise_id)
+        )
         for recruitment in recruitment_list:
             rec_enter_id = recruitment["enterprise_id"]
             enterprise_name = Enterprise.objects.get(id=rec_enter_id).name
             recruitment["enterprise_name"] = enterprise_name
-        return JsonResponse({"errno": 0, "msg": "获取企业招聘信息成功", "data": recruitment_list})
+        return JsonResponse(
+            {"errno": 0, "msg": "获取企业招聘信息成功", "data": recruitment_list}
+        )
     return JsonResponse({"errno": 2001, "msg": "请求方式错误"})
 
 
@@ -99,23 +104,25 @@ def get_enterprise_recruitment(request):
 def creat_enterprise(request):
     if request.method == "POST":
         # 获取请求内容
-        user_id = request.data.get('user_id')
-        name = request.data.get('name')
-        profile = request.data.get('profile')
-        picture = request.FILES['picture']
-        address = request.data.get('address')
+        user_id = request.data.get("user_id")
+        name = request.data.get("name")
+        profile = request.data.get("profile")
+        picture = request.FILES["picture"]
+        address = request.data.get("address")
         # 获取实体
         if not Applicant.objects.filter(id=user_id).exists():
-            return JsonResponse({'errno': 7002, 'msg': "该用户不存在"})
+            return JsonResponse({"errno": 7002, "msg": "该用户不存在"})
         user = Applicant.objects.get(id=user_id)
         if user.manage_enterprise_id != 0 or user.enterprise_id != 0:
-            return JsonResponse({'errno': 7004, 'msg': "该用户非管理员"})
+            return JsonResponse({"errno": 7004, "msg": "该用户非管理员"})
         # 创建实体
-        enterprise = Enterprise.objects.create(name=name, profile=profile, picture=picture, address=address)
+        enterprise = Enterprise.objects.create(
+            name=name, profile=profile, picture=picture, address=address
+        )
         user.manage_enterprise_id = enterprise.id
         user.enterprise_id = enterprise.id
         user.save()
-        return JsonResponse({'error': 0, 'msg': '创建成功'})
+        return JsonResponse({"error": 0, "msg": "创建成功"})
 
     return JsonResponse({"error": 7001, "msg": "请求方式错误"})
 
@@ -124,17 +131,22 @@ def creat_enterprise(request):
 def show_enterprise(request):
     if request.method == "POST":
         # 获取请求内容
-        enterprise_id = request.data.get('enterprise_id')
+        enterprise_id = request.data.get("enterprise_id")
         # 获取实体
         if not Enterprise.objects.filter(id=enterprise_id).exists():
-            return JsonResponse({'errno': 7003, 'msg': "该公司不存在"})
+            return JsonResponse({"errno": 7003, "msg": "该公司不存在"})
         enterprise = Enterprise.objects.get(id=enterprise_id)
         # 返回信息
-        data = {"name": enterprise.name, "profile": enterprise.profile,
-                "picture": enterprise_picture_base64(enterprise.picture),
-                "address": enterprise.address, "manager_id": enterprise.manager.id,
-                "manager_name": enterprise.manager.user_name, "manager_email": enterprise.manager.email}
-        return JsonResponse({'error': 0, 'data': data})
+        data = {
+            "name": enterprise.name,
+            "profile": enterprise.profile,
+            "picture": enterprise_picture_base64(enterprise.picture),
+            "address": enterprise.address,
+            "manager_id": enterprise.manager.id,
+            "manager_name": enterprise.manager.user_name,
+            "manager_email": enterprise.manager.email,
+        }
+        return JsonResponse({"error": 0, "data": data})
 
     return JsonResponse({"error": 7001, "msg": "请求方式错误"})
 
@@ -143,22 +155,22 @@ def show_enterprise(request):
 def update_enterprise(request):
     if request.method == "POST":
         # 获取请求内容
-        user_id = request.data.get('user_id')
-        user_be_manager_id = request.data.get('user_be_manager_id')
-        name = request.data.get('name')
-        profile = request.data.get('profile')
-        picture = request.FILES['picture']
-        address = request.data.get('address')
+        user_id = request.data.get("user_id")
+        user_be_manager_id = request.data.get("user_be_manager_id")
+        name = request.data.get("name")
+        profile = request.data.get("profile")
+        picture = request.FILES["picture"]
+        address = request.data.get("address")
         # 获取实体
         if not Applicant.objects.filter(id=user_id).exists():
-            return JsonResponse({'errno': 7002, 'msg': "该用户不存在"})
+            return JsonResponse({"errno": 7002, "msg": "该用户不存在"})
         if not Applicant.objects.filter(id=user_be_manager_id).exists():
-            return JsonResponse({'errno': 7005, 'msg': "目标用户不存在"})
+            return JsonResponse({"errno": 7005, "msg": "目标用户不存在"})
         user = Applicant.objects.get(id=user_id)
         user_be_manager = Applicant.objects.get(id=user_be_manager_id)
         # 如果用户不是管理员判断
         if user.manage_enterprise_id == 0:
-            return JsonResponse({'errno': 7004, 'msg': "该用户非管理员"})
+            return JsonResponse({"errno": 7004, "msg": "该用户非管理员"})
         enterprise = Enterprise.objects.get(id=user.manage_enterprise_id)
         # 修改实体
         enterprise.name = name
@@ -167,7 +179,7 @@ def update_enterprise(request):
         enterprise.address = address
         enterprise.manager = user_be_manager
         enterprise.save()
-        return JsonResponse({'error': 0, 'msg': '修改成功'})
+        return JsonResponse({"error": 0, "msg": "修改成功"})
 
     return JsonResponse({"error": 7001, "msg": "请求方式错误"})
 
@@ -176,14 +188,14 @@ def update_enterprise(request):
 def delete_enterprise(request):
     if request.method == "POST":
         # 获取请求内容
-        user_id = request.data.get('user_id')
+        user_id = request.data.get("user_id")
         # 获取实体
         if not Applicant.objects.filter(id=user_id).exists():
-            return JsonResponse({'errno': 7002, 'msg': "该用户不存在"})
+            return JsonResponse({"errno": 7002, "msg": "该用户不存在"})
         user = Applicant.objects.get(id=user_id)
         # 如果用户不是管理员判断
         if user.manage_enterprise_id == 0:
-            return JsonResponse({'errno': 7004, 'msg': "该用户非管理员"})
+            return JsonResponse({"errno": 7004, "msg": "该用户非管理员"})
         enterprise = Enterprise.objects.get(id=user.manage_enterprise_id)
         # 执行删除
         users = enterprise.member.all()
@@ -194,9 +206,10 @@ def delete_enterprise(request):
         user.enterprise_id = 0
         enterprise.delete()
         user.save()
-        return JsonResponse({'error': 0, 'msg': '删除成功'})
+        return JsonResponse({"error": 0, "msg": "删除成功"})
 
     return JsonResponse({"error": 7001, "msg": "请求方式错误"})
+
 
 #
 # @csrf_exempt
@@ -238,37 +251,16 @@ def enterprise_picture_base64(request, picture):
     image = Image.open(picture)
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
-    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return image_base64
 
 
 def to_json_member(member):
-    info = {
-        "user_id": member
-
-    }
+    info = {"user_id": member}
     return json.dumps(info)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # def get_enterprise_picture_url(request, enterprise_id):
 #     enterprise = get_object_or_404(Enterprise, id=enterprise_id)
 #     picture_url = enterprise.picture.url if enterprise.picture else None
 #     return JsonResponse({'picture_url': picture_url})
-
