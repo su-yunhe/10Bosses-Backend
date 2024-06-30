2f2677446d9145adcb1d8ae282379f089b9c8437
Rylynn <2694561878@qq.com>
2024-06-30 12:48:41 +0800

    通知应用更新

--- notification/urls.py
+++ notification/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("employee_quit_notification", employee_quit_notification),
     path("resume_notification", resume_notification),
     path("user_reply_notification", user_reply_notification),
+    path("get_notification_list", get_notification_list),
+    path("get_notification_detail", get_notification_detail),
+    path("delete_notification", delete_notification),
 ]
